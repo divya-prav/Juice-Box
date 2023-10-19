3b77438c15c1eab122fd63203546a6ad3af4784f
divya040392 <divya.praveen.5215@gmail.com>
2023-10-19 16:16:22 -0500

    Initial commit

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const { Client } = require("pg"); // imports the pg module
 const client = new Client({
   connectionString:
     process.env.DATABASE_URL ||
-    "postgres://postgres:anish123@localhost:5432/juicebox-dev",
+    "https://localhost:5432/juicebox-dev",
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
